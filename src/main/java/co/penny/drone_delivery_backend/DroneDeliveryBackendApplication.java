130b6576637b2fd858ebbf9be36e26d02ada1ce7
Shihaaz Buhary <shihaazbz@gmail.com>
2025-12-10 11:29:18 +0530

    initial project scaffold

---
+++ src/main/java/co/penny/drone_delivery_backend/DroneDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package co.penny.drone_delivery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/penny/drone_delivery_backend/DroneDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.penny.drone_delivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
